7d599b07703e2d972429eee4bf3959eaaea70a0f
dextroseSH <pheinrich11235@gmail.com>
2021-07-04 21:15:31 +0200

    changed noise mock and add structure for jwt

--- index.js
+++ index.js
@@ -11,25 +11,72 @@ export class NoiseDirective extends SchemaDirectiveVisitor{
 
     visitFieldDefinition(field){
         const {resolve = defaultFieldResolver} = field;
-        const argumentsForNoise = this.getArgumentForRole();
+        const noiseArgumentsOnRole = this.getArgumentForRoles();
         field.resolve = async function(result, args, context, info){
             const res = await resolve.apply(this,[result, args, context, info]);
             //in context -> req -> req.headers -> req.authorization || req.cookies.token 
             //evaluate role (https://github.com/grand-stack/graphql-auth-directives/blob/master/src/index.js)
-            console.log(argumentsForNoise);
+            
+            /*const token = verifyAndDecodeToken(context);
+            const role = token.role;
 
-            return addNoise(res, {
+            noiseArgument = noiseArgumentsOnRole[role];
+            if(!noiseArgument) throw new Error("There is no noise argument specified for this role.")*/
+
+
+
+            /*return addNoise(res, {
                 typeOfDistribution:"uniformInt", 
                 distributionParameters:{
                     max: 100,
                     min: 100,
                 }, 
                 valueParameters:{
-                    isInt: true,
+                    isInt: false,
+                }
+            })*/
+
+            return addNoise(res, {
+                typeOfDistribution:"normal", 
+                distributionParameters:{
+                    mean: 0,
+                    standardDeviation: 1,
+                }, 
+                valueParameters:{
+                    isInt: false,
                 }
             })
         }
     }
 }
+
+const verifyAndDecodeToken = function(context){
+    
+    if(!context || !context.req || !context.req.headers || !context.req.headers.Authorization) {
+        throw new Error("No authorization token.");
+    }
+  
+    const token = req.headers.authorization;
+
+    try {
+        //const id_token = token.replace("Bearer ", "");
+        const { JWT_SECRET, JWT_NO_VERIFY } = process.env;
+  
+        if (!JWT_SECRET && JWT_NO_VERIFY) {
+            return jwt.decode(id_token);
+        } else {
+            return jwt.verify(id_token, JWT_SECRET, {
+                algorithms: ["HS256", "RS256"]
+            });
+        }
+    } catch (err) {
+        if (err.name === "TokenExpiredError") {
+            throw new Error("Your token is expired.");
+        } else {
+            throw new Error("Could not verify and decode token.");
+      }
+    }
+};
+  
 
 //add other directives
